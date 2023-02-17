352596c9528f840325903169d5242ac5c0251a6e
Radekovar <radekovar@gmail.com>
2023-02-17 12:19:11 +0100

    Přidaní všech podmínek pro c-instrukce

--- hack.py
+++ hack.py
@@ -1,20 +1,98 @@
-dest= input
-jump = input
-if dest == True:
-    if dest =='M':
-        poloha=('001')
-    else:
-        poloha=('000') 
-elif dest == False:
-    poloha = ('000')
+c_inst = input()
+# instrukce0
+if '0' in c_inst:
+    comp = ('0101010')
+if '1' in c_inst:
+    comp = ('0111111')
+if '-1' in c_inst:
+    comp = ('0111010')
+if 'D' in c_inst and not 'D=' in c_inst:
+    comp = ('0001100')    
+if 'A' in c_inst and not 'A=' in c_inst:
+    comp = ('0110000')
+if 'M' in c_inst and not 'M=' in c_inst:
+    comp = ('1110000')
+if '!D' in c_inst:
+    comp = ('0001101')
+if '!A' in c_inst:
+    comp = ('0110001')
+if '!M' in c_inst:
+    comp = ('1110001')
+if '-D' in c_inst:
+    comp = ('0001111')
+if '-A' in c_inst:
+    comp = ('0110011')
+if '-M' in c_inst:
+    comp = ('1110011')
+if 'D+1' in c_inst:
+    comp = ('1011111')
+if 'A+1' in c_inst:
+    comp = ('0110111')
+if 'M+1' in c_inst:
+    comp = ('1110111')
+if 'D-1' in c_inst:
+    comp = ('0001110')
+if 'A-1' in c_inst:
+    comp = ('0110010')
+if 'M-1' in c_inst:
+    comp = ('1110010')
+if 'D+A' in c_inst:
+    comp = ('0000010')
+if 'D+M' in c_inst:
+    comp = ('1000010')
+if 'D-A' in c_inst:
+    comp = ('0010011')
+if 'D-M' in c_inst:
+    comp = ('1010011')
+if 'A-D' in c_inst:
+    comp = ('0000111')
+if 'M-D' in c_inst:
+    comp = ('1000111')
+if 'D&A' in c_inst:
+    comp = ('0000000')
+if 'D&M' in c_inst:
+    comp = ('1000000')    
+if 'D|A' in c_inst:
+    comp = ('0010101')
+if 'D|M' in c_inst:
+    comp = ('1010101')
 
-    # Skoky
-if jump == True:
-    if jump =='JGT':
-        jump_=('001')
-    else:
-        jump_=('000')
-elif jump == False:
-    jump_=('000')
-print (poloha)
-print (jump_)
+# Destinace
+if 'M=' in c_inst:
+    dest = ('001')
+elif 'D=' in c_inst:
+    dest = ('010')
+elif 'MD=' in c_inst:
+    dest = ('011')
+elif 'A=' in c_inst:
+    dest = ('100')
+elif 'AM=' in c_inst:
+    dest = ('101')
+elif 'AD=' in c_inst:
+    dest = ('110')
+elif 'AMD=' in c_inst:
+    dest = ('111')
+
+else:
+    dest = ('000')
+
+# Jump
+if 'JGT' in c_inst:
+    jump=('001')
+elif 'JEQ' in c_inst:
+    jump=('010')
+elif 'JGE' in c_inst:
+    jump=('011')
+elif 'JLT' in c_inst:
+    jump=('100')
+elif 'JNE' in c_inst:
+    jump=('101')
+elif 'JLE' in c_inst:
+    jump=('110')
+elif 'JMP' in c_inst:
+    jump=('111')
+else:
+    jump=('000')
+
+print(111,comp,dest,jump,sep='')
+
